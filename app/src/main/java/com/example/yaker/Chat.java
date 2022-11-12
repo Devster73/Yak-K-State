7e604df9fb91c695429f158e3176db28aa324b5a
Devin Richards <devin.a.rich@gmail.com>
2022-11-12 14:29:14 -0600

    Only changed Chat class

--- app/src/main/java/com/example/yaker/Chat.java
+++ app/src/main/java/com/example/yaker/Chat.java
@@ -1,14 +1,15 @@
 package com.example.yaker;
 
+import java.util.List;
+
 public class Chat {
     String chatMessage;
+    List<String> comments;
     int likes;
 
     public String getChat(){
         return chatMessage;
     }
-    public String getLikes(){
-
-        return String.valueOf(likes);
-    }
+    public String getLikes() { return String.valueOf(likes); }
+    public List<String> getComments() { return comments; }
 }
